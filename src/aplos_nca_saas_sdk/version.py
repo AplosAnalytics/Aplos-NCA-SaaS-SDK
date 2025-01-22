006542ab72c7d15cbaeb73bf4a7e0677af21a46c
Eric Wilson <eric.wilson@aplosanalytics.com>
2025-01-22 18:20:04 -0500

    👉 release (0.0.14)

--- src/aplos_nca_saas_sdk/version.py
+++ src/aplos_nca_saas_sdk/version.py
@@ -1,4 +1,4 @@
 # Aplos NCA SaaS SDK Version File
 # This is automatically generated during the build process. 
 # DO NOT UPDATE IT DIRECTLY. IT WILL BE OVERWRITTEN. 
-__version__ = '0.0.13'
+__version__ = '0.0.14'
